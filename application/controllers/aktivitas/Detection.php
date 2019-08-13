d7bca89908f3bdaa23a71e777c2a70a33d25d6d0
DannyTan <alecia_clair00@yahoo.com>
2019-08-13 16:14:34 +0700

    fix detection

--- application/controllers/aktivitas/Detection.php
+++ application/controllers/aktivitas/Detection.php
@@ -39,70 +39,64 @@ class Detection extends MY_Controller {
 				// non fraud
 				$payment = $this->detectionModel->getAllPayment();
 				$transaction = $this->detectionModel->getAllTransaction();
-				$report = $this->detectionModel->getAllReport();
+				// $report = $this->detectionModel->getAllReport();
 				//fraud
 				$paymentFraud = $this->detectionModel->getPaymentFraud($item->id_user);
 				$transactionFraud = $this->detectionModel->getTransactionFraud($item->id_user);
-				$reportFraud = $this->detectionModel->getReportFraud($item->id_user);
+				// $reportFraud = $this->detectionModel->getReportFraud($item->id_user);
+				$crediCardFraud = $this->detectionModel->getCreditCardFraud($item->username);
 				//total Data
+				$persenUserbehavior = 0;
+				$creditName = 0;
+				$creditDelete = 0;
+				$creditFraudUser = 0;
+				foreach ($crediCardFraud as $key => $credit) {
+					if (strpos($credit->card_user, $credit->card_name) !== true) {
+					    $creditName = 0.33;
+					}
+					if ($credit->status == 0) {
+						$creditDelete = 0.33;
+					}
+					if ($credit->card_fraud_user != 0) {
+						$creditFraudUser = 0.33;
+					}
+				}
+				$persenUserbehaviorT = $creditName + $creditDelete + $creditFraudUser;
+				$persenUserbehaviorF = 1 - ($creditName + $creditDelete + $creditFraudUser);
 
 				$totalPayment = $payment + $paymentFraud;
 				$totalTransaction = $transaction + $transactionFraud;
-				$totalReport = $report + $reportFraud;
+				// $totalReport = $report + $reportFraud;
 				// persentase
 				$persenPayment = $payment / $totalPayment;
 				$persenPaymentFraud = $paymentFraud / $totalPayment;
 
-				$persenTransaction = $transaction / $totalTransaction;
-				$persenTransactionFraud = $transactionFraud / $totalTransaction;
-
-				$persenReport = $report / $totalReport;
-				$persenReportFraud = $reportFraud / $totalReport;
-				// var_dump($persenPayment." ".$persenTransaction." ". $persenReport);
-				// IP = IP & ip,bulkbuy,payment
-
-				$ipTTT = 1;
-				$ipTTF = 0;
-				$ipTFT = 1;
-				$ipTFF = 0;
-				$ipFTT = 0;
-				$ipFTF = 0.1;
-				$ipFFT = 0.9;
-				$ipFFF = 1;
+				// $persenTransaction = $transaction / $totalTransaction;
+				// $persenTransactionFraud = $transactionFraud / $totalTransaction;
+				//
+				// $persenReport = $report / $totalReport;
+				// $persenReportFraud = $reportFraud / $totalReport;
 
 				//Trans = Transaksi  & transaksi,ip,report
 
-				$transTTT = 1;
-				$transTTF = 0;
-				$transTFT = 1;
-				$transTFF = 0;
-				$transFTT = 0;
-				$transFTF = 0.1;
-				$transFFT = 0.9;
-				$transFFF = 1;
+				$transTTT = 0.9;
+				$transTTF = 0.1;
+				$transTFT = 0.2;
+				$transTFF = 0.8;
+				$transFTT = 0.3;
+				$transFTF = 0.7;
+				$transFFT = 0.7;
+				$transFFF = 0.3;
 
 				//P(bb,p,ip,ur,t) = P (BB) x P (p) x P (ur) x P (ip | p , b) x P ( t |ur , ip )
 
-				$fraudTF000 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipFFF * $transTFF;
-				$fraudTF001 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipFFF * $transTFT;
-				$fraudTF010 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipTFF * $transTTF;
-				$fraudTF011 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipTFF * $transTTT;
-				$fraudTF100 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipFTF * $transTFF;
-				$fraudTF101 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipFTF * $transTFT;
-				$fraudTF110 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipTTF * $transTTF;
-				$fraudTF111 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipTTF * $transTTT;
+				$fraudTT000 = $transFTT * $persenPaymentFraud * $persenUserbehaviorF;
+				$fraudTT001 = $transTTT * $persenPaymentFraud * $persenUserbehaviorT;
+				$fraudTT010 = $transFTF * $persenPaymentFraud * $persenUserbehaviorF;
+				$fraudTT011 = $transTTF * $persenPaymentFraud * $persenUserbehaviorT;
 
-				$fraudTT000 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipFFT * $transTFF;
-				$fraudTT001 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipFFT * $transTFT;
-				$fraudTT010 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipTFT * $transTTF;
-				$fraudTT011 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipTFT * $transTTT;
-				$fraudTT100 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipFTT * $transTFF;
-				$fraudTT101 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipFTT * $transTFT;
-				$fraudTT110 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipTTT * $transTTF;
-				$fraudTT111 = $persenTransactionFraud * $persenPaymentFraud * $persenReportFraud * $ipTTT * $transTTT;
+				$total1 = $fraudTT000 + $fraudTT001 + $fraudTT010 + $fraudTT011;
 
-				$total1 = $fraudTF000 + $fraudTF001 + $fraudTF010 + $fraudTF011 + $fraudTF100 + $fraudTF101 + $fraudTF110 + $fraudTF111;
-				$total2 = $fraudTT000 +	$fraudTT001 +	$fraudTT010 +	$fraudTT011 +	$fraudTT100 +	$fraudTT101 +	$fraudTT110 +	$fraudTT111;
 				$listTransaksi = $this->detectionModel->listTransaction($item->id_user);
 				$stringTrans = "";
 				$counter = 0;
@@ -123,18 +117,18 @@ class Detection extends MY_Controller {
 				if ($paymentFraud > 0) {
 					$stringFraudcase .= "Payment,";
 				}
-				if ($transactionFraud > 0) {
-					$stringFraudcase .= "Bulk Buy,";
-				}
-				if ($reportFraud > 0) {
-					$stringFraudcase .= "Report";
+				// if ($transactionFraud > 0) {
+				// 	$stringFraudcase .= "User Behavior,";
+				// }
+				if ($persenUserbehaviorT > 0) {
+					$stringFraudcase .= "User Behavior";
 				}
 
-				if (($total1 == 0 || $total2 == 0) || ($total1 == 0 && $total2 == 0)) {
+				if ($total1 == 0) {
 					$total3 = 0;
 				}
 				else {
-					$total3 = $total2/($total1 + $total2);
+					$total3 = $total1;
 				}
 				$item->transaction_limit = "Rp.".number_format($item->transaction_limit,0,",",",");
 				$item->Fraudcase = $stringFraudcase;
